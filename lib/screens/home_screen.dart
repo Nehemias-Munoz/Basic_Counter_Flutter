875eab191e5021349d43c43070c8ac2f54a5aa98
Nehemias Munoz <nehemiasmunozm@gmail.com>
2022-12-09 13:01:29 -0300

    Estructura visual lista

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,8 +5,52 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const textStyle = TextStyle(fontSize: 30.0);
     return Scaffold(
-      body: Center(child: const Text('HomeScreen')),
+      appBar: AppBar(
+        elevation: 0.5,
+        centerTitle: true,
+        title: const Text("Counter App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Click Counter:",
+              style: textStyle,
+            ),
+            Text(
+              "0",
+              style: textStyle,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            FloatingActionButton(
+              elevation: 0.6,
+              onPressed: () {},
+              child: const Icon(Icons.add),
+            ),
+            FloatingActionButton(
+              elevation: 0.6,
+              onPressed: () {},
+              child: const Icon(Icons.restart_alt),
+            ),
+            FloatingActionButton(
+              elevation: 0.6,
+              onPressed: () {},
+              child: const Icon(Icons.remove),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
