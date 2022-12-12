42daa98bb0de83efe9bcb729c28265a218c124f3
Nehemias Munoz <nehemiasmunozm@gmail.com>
2022-12-12 14:07:50 -0300

    Terminada logica contador

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int counter = 0;
+
+  void increase() => setState(() => counter++);
+  void decrease() => setState(() => counter--);
+  void reset() => setState(() => counter = 0);
   @override
   Widget build(BuildContext context) {
     const textStyle = TextStyle(fontSize: 30.0);
@@ -21,7 +31,7 @@ class HomeScreen extends StatelessWidget {
               style: textStyle,
             ),
             Text(
-              "0",
+              counter.toString(),
               style: textStyle,
             ),
           ],
@@ -30,27 +40,49 @@ class HomeScreen extends StatelessWidget {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            FloatingActionButton(
-              elevation: 0.6,
-              onPressed: () {},
-              child: const Icon(Icons.add),
-            ),
-            FloatingActionButton(
-              elevation: 0.6,
-              onPressed: () {},
-              child: const Icon(Icons.restart_alt),
-            ),
-            FloatingActionButton(
-              elevation: 0.6,
-              onPressed: () {},
-              child: const Icon(Icons.remove),
-            ),
-          ],
+        child: CustomFloatingActionBar(
+          decreaseFn: decrease,
+          increaseFn: increase,
+          resetFn: reset,
         ),
       ),
+    );
+  }
+}
+
+class CustomFloatingActionBar extends StatelessWidget {
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function resetFn;
+
+  const CustomFloatingActionBar({
+    Key? key,
+    required this.increaseFn,
+    required this.decreaseFn,
+    required this.resetFn,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        FloatingActionButton(
+          elevation: 0.6,
+          onPressed: () => decreaseFn(),
+          child: const Icon(Icons.remove),
+        ),
+        FloatingActionButton(
+          elevation: 0.6,
+          onPressed: () => resetFn(),
+          child: const Icon(Icons.restart_alt),
+        ),
+        FloatingActionButton(
+          elevation: 0.6,
+          onPressed: () => increaseFn(),
+          child: const Icon(Icons.add),
+        ),
+      ],
     );
   }
 }
